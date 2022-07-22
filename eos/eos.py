125358a6930436467a9516e3b169fd101d714ef7
Jingyao Dou <qb20321@bristol.ac.uk>
2022-07-22 23:57:43 +0100

    reset 3 commit

--- eos/eos.py
+++ eos/eos.py
@@ -346,7 +346,11 @@ def P_T_rho(T, rho, mat_id):
     """
     mat_type = mat_id // gv.type_factor
     if mat_type == gv.type_idg:
+<<<<<<< HEAD
         P = idg.P_T_rho(T, rho, mat_id) ### P_u_rho instead
+=======
+        P = idg.P_T_rho(T, rho, mat_id)
+>>>>>>> jy_dev
     elif mat_type == gv.type_Til:
         P = tillotson.P_T_rho(T, rho, mat_id)
     elif mat_type == gv.type_HM80:
@@ -420,6 +424,7 @@ def T_u_rho(u, rho, mat_id):
     T : float
         Temperature (K).
     """
+<<<<<<< HEAD
 
     mat_type = mat_id // gv.type_factor
     
@@ -434,6 +439,17 @@ def T_u_rho(u, rho, mat_id):
     elif mat_type in [gv.type_SESAME, gv.type_ANEOS]:
         T = sesame.T_u_rho(u, rho, mat_id)
         
+=======
+    mat_type = mat_id // gv.type_factor
+    if mat_type == gv.type_idg:
+        T = idg.T_u_rho(u, rho, mat_id)
+    elif mat_type == gv.type_Til:
+        T = tillotson.T_u_rho(u, rho, mat_id)
+    elif mat_type == gv.type_HM80:
+        T = hm80.T_u_rho(u, rho, mat_id)
+    elif mat_type in [gv.type_SESAME, gv.type_ANEOS]:
+        T = sesame.T_u_rho(u, rho, mat_id)
+>>>>>>> jy_dev
     else:
         raise ValueError("Invalid material ID")
     return T
@@ -460,18 +476,30 @@ def A1_T_u_rho(A1_u, A1_rho, A1_mat_id):
     A1_T : [float]
         Temperature (K).
     """
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> jy_dev
     assert A1_u.ndim == 1
     assert A1_rho.ndim == 1
     assert A1_mat_id.ndim == 1
     assert A1_u.shape[0] == A1_rho.shape[0]
     assert A1_u.shape[0] == A1_mat_id.shape[0]
+<<<<<<< HEAD
     
     A1_T = np.zeros_like(A1_u)
    
     for i, u in enumerate(A1_u):
         A1_T[i] = T_u_rho(A1_u[i], A1_rho[i], A1_mat_id[i])
         
+=======
+
+    A1_T = np.zeros_like(A1_u)
+
+    for i, u in enumerate(A1_u):
+        A1_T[i] = T_u_rho(A1_u[i], A1_rho[i], A1_mat_id[i])
+>>>>>>> jy_dev
 
     return A1_T
 
@@ -511,7 +539,11 @@ def u_rho_T(rho, T, mat_id):
     else:
         raise ValueError("Invalid material ID")
     return u
+<<<<<<< HEAD
   #ss
+=======
+
+>>>>>>> jy_dev
 
 @njit
 def A1_u_rho_T(A1_rho, A1_T, A1_mat_id):
@@ -552,7 +584,11 @@ def A1_u_rho_T(A1_rho, A1_T, A1_mat_id):
 # Specific entropy
 # ========
 @njit
+<<<<<<< HEAD
 def s_rho_T(rho, T, P, mat_id):
+=======
+def s_rho_T(rho, T, mat_id):
+>>>>>>> jy_dev
     """Compute the specific entropy from the density and temperature, for any EoS.
 
     Parameters
@@ -577,15 +613,22 @@ def s_rho_T(rho, T, P, mat_id):
     elif mat_id==200:
         #print('set HM80 HHe entropy to zero')
         s=0
+<<<<<<< HEAD
     elif mat_id==0:
         s=idg.s_P_rho(P, rho, mat_id)
+=======
+>>>>>>> jy_dev
     else:
         raise ValueError("Entropy not implemented for this material type.")
     return s
 
 
 @njit
+<<<<<<< HEAD
 def A1_s_rho_T(A1_rho, A1_T, A1_P, A1_mat_id):
+=======
+def A1_s_rho_T(A1_rho, A1_T, A1_mat_id):
+>>>>>>> jy_dev
     """Compute the specific entropies from arrays of density and temperature,
     for any EoS.
 
@@ -614,7 +657,11 @@ def A1_s_rho_T(A1_rho, A1_T, A1_P, A1_mat_id):
     A1_s = np.zeros_like(A1_T)
 
     for i, rho in enumerate(A1_rho):
+<<<<<<< HEAD
         A1_s[i] = s_rho_T(A1_rho[i], A1_T[i], A1_P[i], A1_mat_id[i])
+=======
+        A1_s[i] = s_rho_T(A1_rho[i], A1_T[i], A1_mat_id[i])
+>>>>>>> jy_dev
 
     return A1_s
 
@@ -643,6 +690,12 @@ def s_u_rho(u, rho, mat_id):
     mat_type = mat_id // gv.type_factor
     if mat_type in [gv.type_SESAME, gv.type_ANEOS]:
         s = sesame.s_u_rho(u, rho, mat_id)
+<<<<<<< HEAD
+=======
+    elif mat_id==0:
+        #print('set HM80 HHe entropy to zero')
+        s = idg.s_u_rho(u, rho, mat_id)
+>>>>>>> jy_dev
     else:
         raise ValueError("Entropy not implemented for this material type.")
     return s
@@ -787,7 +840,11 @@ def find_rho(P_des, mat_id, T_rho_type, T_rho_args, rho_min, rho_max):
     elif P_des < P_max < P_min:
         return rho_max
     else:
+<<<<<<< HEAD
         #For debugging
+=======
+        # For debugging
+>>>>>>> jy_dev
         print('P_des:',P_des)
         print('mat_id:',mat_id)
         print('T_rho_type_args',T_rho_type, T_rho_args)
